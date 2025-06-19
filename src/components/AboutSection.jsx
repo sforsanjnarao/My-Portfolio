5f194518b1bfa12f2da7d82dc1d08bacbd19ab60
sforsanjnarao <sanjnarao26@gmail.com>
2025-06-19 14:18:49 +0530

    fix AboutSection: update CV link to point to the correct Google Drive file

--- src/components/AboutSection.jsx
+++ src/components/AboutSection.jsx
@@ -34,7 +34,7 @@ export const AboutSection = () => {
               </a>
 
               <a
-                href=""
+                href="https://drive.google.com/file/d/1VzvUEzykBCWAXKxz8IHc62QLUeO9D0zb/view?usp=sharing"
                 className="px-6 py-2 rounded-full border border-primary text-primary hover:bg-primary/10 transition-colors duration-300"
               >
                 My CV
